fe972d7859d80d979003f40558561eb9d5dfdf08
ezz2003 <ezz2003@yandex.ru>
2023-02-19 20:08:46 +1000

    solve the problem18

---
+++ problem18/Program.cs
@@ -0,0 +1,20 @@
+﻿// 18. Напишите программу, которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (Х и Y)
+
+int InputInt(string mes)
+{
+  Console.Write(mes);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+string GetCoordinatesRange(int quart)
+{
+  string temp = $"Диапазон значений {quart} четверти -- ";
+  if (quart == 1) return $" {temp} x > 0, y > 0";
+  if (quart == 2) return $" {temp} x < 0, y > 0";
+  if (quart == 3) return $" {temp} x < 0, y < 0";
+  if (quart == 4) return $" {temp} x > 0, y < 0";
+  return "Недопустимая четверть";
+}
+
+int quarter = InputInt("Введите номер четверти координатной плоскости: ");
+Console.WriteLine(GetCoordinatesRange(quarter));
