c899f1a95e1cee5cc07bc1d7dbac3bbee94e4658
ezz2003 <ezz2003@yandex.ru>
2023-02-19 18:11:42 +1000

    solving the problem17

--- problem17/Program.cs
+++ problem17/Program.cs
@@ -2,3 +2,21 @@
 // 
 // 
 
+int Quarter(int x, int y)
+{
+  if(x > 0 && y > 0) return 1;
+  if(x < 0 && y > 0) return 2;
+  if(x < 0 && y < 0) return 3;
+  if(x > 0 && y < 0) return 4;
+  return 0;
+}
+
+
+Console.Write("Введите координату Х: ");
+int xc = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+int yc = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(xc, yc);
+string answer = quarter > 0 ? $"Указаны координаты {quarter} четверти" : $"Введены недопустимые координаты";
+Console.WriteLine(answer);
