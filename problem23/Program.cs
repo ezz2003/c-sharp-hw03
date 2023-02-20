d0be72b2bf47453c3142a228402d263b1e1303b5
ezz2003 <ezz2003@yandex.ru>
2023-02-20 19:28:08 +1000

    solved the problem23

---
+++ problem23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23
+// 
+// // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void PrintThirdPowerNumbers(int n)
+{ 
+  if (n < 0) n = n * -1;
+  for (int i = 1; i <= n; i++)
+  {
+    Console.WriteLine($"{i, 5} {Math.Pow(i, 3), 15}");
+  }
+}
+
+Console.WriteLine("Программа выведет таблицу кубов (третьей степени) чисел от 1 до заданного Вами числа");
+Console.Write("Задайте число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+PrintThirdPowerNumbers(number);
